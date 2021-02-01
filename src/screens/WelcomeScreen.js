e3de6a86e5ba03d08b97a2cd9d851d20e20f9323
Emre Telli <emretelli2002@hotmail.com>
2021-02-01 16:45:48 +0300

    11th Commit 3.0

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -34,7 +34,8 @@ class WelcomeScreen extends Component {
       'https://opentdb.com/api.php?amount=15&category=' +
       this.state.category +
       '&difficulty=' +
-      this.state.difficulty;
+      this.state.difficulty +
+      '&type=multiple';
     console.log(URL);
     const {increasePageNum} = this.props;
     this.setState({
@@ -157,15 +158,56 @@ class WelcomeScreen extends Component {
                           this.setState({category: value})
                         }
                         items={[
-                          {label: 'General Knowledge', value: 9},
+                          {label: 'Entertainment: General Knowledge', value: 9},
                           {label: 'Entertainment: Books', value: 10},
                           {label: 'Entertainment: Film', value: 11},
+                          {label: 'Entertainment: Music', value: 12},
+                          {
+                            label: 'Entertainment: Musicals & Theatres',
+                            value: 13,
+                          },
+                          {label: 'Entertainment: Television', value: 14},
+                          {label: 'Entertainment: Video Games', value: 15},
+                          {label: 'Entertainment: Board Games', value: 16},
+                          {label: 'Science & Nature', value: 17},
+                          {label: 'Science: Computers', value: 18},
+                          {label: 'Science: Mathematics', value: 19},
+                          {label: 'Mythology', value: 20},
+                          {label: 'Sports', value: 21},
+                          {label: 'Geography', value: 22},
+                          {label: 'History', value: 23},
+                          {label: 'Politics', value: 24},
+                          {label: 'Art', value: 25},
+                          {label: 'Celebrities', value: 26},
+                          {label: 'Animals', value: 27},
+                          {label: 'Vehicles', value: 28},
+                          {label: 'Entertainment: Comics', value: 29},
+                          {label: 'Science: Gadgets', value: 30},
+                          {
+                            label: 'Entertainment: Japanese Anime & Manga',
+                            value: 31,
+                          },
+                          {
+                            label: 'Entertainment: Cartoon & Animations',
+                            value: 32,
+                          },
                         ]}
                       />
                     </Text>
                     <Text
                       style={{marginTop: 10, fontSize: 24, fontWeight: 'bold'}}>
-                      Difficulty: Medium
+                      Difficulty:{' '}
+                      <RNPickerSelect
+                        style={{fontSize: 24, fontWeight: 'bold'}}
+                        onValueChange={(value) =>
+                          this.setState({difficulty: value})
+                        }
+                        items={[
+                          {label: 'Easy', value: 'easy'},
+                          {label: 'Medium', value: 'medium'},
+                          {label: 'Hard', value: 'hard'},
+                        ]}
+                      />
                     </Text>
                   </View>
                 </View>
